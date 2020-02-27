70a2ab1ba1f9f021e1f2715f7ca868fa57d891f8
Tjalle <tjalle@tjallo.nl>
2020-02-27 22:03:23 +0100

    boilerplate code

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-import flask
+from flask import Flask, render_template, request
 from sources import secret
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/lastfm/')
+def lastfm():
+    return render_template('lastfm.html')
+
+if __name__ == "__main__":
+    app.run(debug =True)
